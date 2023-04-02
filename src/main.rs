e87c35b0532879e088d6fa4beadfc6b932f71985
Masterchef365 <duncan.freeman1@gmail.com>
2023-04-01 18:16:26 -0700

    Start thinking about particle shader

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+extern crate glow as gl;
 use anyhow::{bail, format_err, Context as AnyhowContext, Result};
-use glow::*;
+use gl::HasContext;
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::ControlFlow;
+
+const N_PARTICLES: i32 = 10_000;
+const WIDTH: i32 = 500;
+const HEIGHT: i32 = 500;
 
 fn main() -> Result<()> {
     unsafe {
@@ -16,31 +21,47 @@ fn main() -> Result<()> {
             .unwrap()
             .make_current()
             .unwrap();
-        let gl = glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
+        let gl = gl::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
 
         // Particle vertex array
-        let vertex_array = gl
+        let particle_vertex_array = gl
             .create_vertex_array()
             .expect("Cannot create vertex array");
-        gl.bind_vertex_array(Some(vertex_array));
+        gl.bind_vertex_array(Some(particle_vertex_array));
+
+        // Particle buffer
+        let particle_buffer = gl.create_buffer().map_err(|e| format_err!("{}", e))?;
+        gl.bind_buffer(gl::ARRAY_BUFFER, Some(particle_buffer));
+        gl.buffer_data_size(
+            gl::ARRAY_BUFFER,
+            N_PARTICLES * std::mem::size_of::<f32>() as i32 * 2,
+            gl::DYNAMIC_DRAW,
+        );
+        gl.bind_vertex_array(None);
 
         // Set up fragment/vertex shaders
         let shader_sources = [
-            (glow::VERTEX_SHADER, include_str!("shaders/particles.vert")),
-            (
-                glow::FRAGMENT_SHADER,
-                include_str!("shaders/particles.frag"),
-            ),
+            (gl::VERTEX_SHADER, include_str!("shaders/particles.vert")),
+            (gl::FRAGMENT_SHADER, include_str!("shaders/particles.frag")),
         ];
+        let particle_shader = create_program(&gl, &shader_sources)?;
 
-        let program = create_program(&gl, &shader_sources)?;
+        let particle_kernel = create_program(&gl, &[(gl::COMPUTE_SHADER, include_str!("kernels/particles.comp"))])?;
 
-        gl.use_program(Some(program));
+        // Set up textures
+        let read_texture = gl.create_texture().map_err(|e| format_err!("{}", e))?;
+        gl.bind_texture(gl::TEXTURE_2D, Some(read_texture));
+        gl.tex_image_2d(gl::TEXTURE_2D, 0, gl::RG32F as _, WIDTH, HEIGHT, 0, gl::RG, gl::FLOAT, None);
+
+        let write_texture = gl.create_texture().map_err(|e| format_err!("{}", e))?;
+        gl.bind_texture(gl::TEXTURE_2D, Some(write_texture));
+        gl.tex_image_2d(gl::TEXTURE_2D, 0, gl::RG32F as _, WIDTH, HEIGHT, 0, gl::RG, gl::FLOAT, None);
+
+        // Set up GL state 
         gl.clear_color(0., 0., 0., 1.0);
-        gl.enable(VERTEX_PROGRAM_POINT_SIZE);
+        gl.enable(gl::VERTEX_PROGRAM_POINT_SIZE);
 
-        // We handle events differently between targets
-
+        // Event loop
         event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Wait;
             match event {
@@ -51,8 +72,23 @@ fn main() -> Result<()> {
                     window.window().request_redraw();
                 }
                 Event::RedrawRequested(_) => {
-                    gl.clear(glow::COLOR_BUFFER_BIT);
-                    gl.draw_arrays(glow::POINTS, 0, 3);
+                    // Execute particle kernel
+                    gl.use_program(Some(particle_kernel));
+                    // Set dt
+                    let dt_loc = gl.get_uniform_location(particle_kernel, "dt");
+                    gl.uniform_1_f32(dt_loc.as_ref(), 0.);
+                    // Set particle buffer
+                    gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 0, Some(particle_buffer));
+                    // Set read texture
+                    gl.bind_texture(gl::TEXTURE0, Some(read_texture));
+                    gl.dispatch_compute(N_PARTICLES as u32, 1, 1);
+
+                    // Draw particles
+                    gl.clear(gl::COLOR_BUFFER_BIT);
+                    gl.use_program(Some(particle_shader));
+                    gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 0, Some(particle_buffer));
+                    gl.bind_texture(gl::TEXTURE0, Some(read_texture));
+                    gl.draw_arrays(gl::POINTS, 0, N_PARTICLES);
                     window.swap_buffers().unwrap();
                 }
                 Event::WindowEvent { ref event, .. } => match event {
@@ -60,8 +96,8 @@ fn main() -> Result<()> {
                         window.resize(*physical_size);
                     }
                     WindowEvent::CloseRequested => {
-                        gl.delete_program(program);
-                        gl.delete_vertex_array(vertex_array);
+                        gl.delete_program(particle_shader);
+                        gl.delete_vertex_array(particle_vertex_array);
                         *control_flow = ControlFlow::Exit
                     }
                     _ => (),
@@ -73,7 +109,10 @@ fn main() -> Result<()> {
 }
 
 /// Compile and link program from sources
-pub fn create_program(gl: &Context, shader_sources: &[(u32, &str)]) -> Result<NativeProgram> {
+pub fn create_program(
+    gl: &gl::Context,
+    shader_sources: &[(u32, &str)],
+) -> Result<gl::NativeProgram> {
     unsafe {
         let program = gl
             .create_program()
--- src/shaders/particles.vert
+++ src/shaders/particles.vert
@@ -1,11 +1,12 @@
 #version 450
-uniform layout(binding=1, rg32f) image2D write_img;
-layout(std430, binding=0) buffer Particles {
+
+uniform layout(binding=0, rg32f) image2D read_img;
+layout(std430, binding=1) buffer Particles {
     vec2 particles[];
 };
 
 void main() {
-    vec2 vert = particles[gl_VertexID] / vec2(imageSize(write_img));
+    vec2 vert = particles[gl_VertexID] / vec2(imageSize(read_img));
     vert = vert * 2. - 1.;
     gl_Position = vec4(vert, 0.0, 1.0);
     gl_PointSize = 5.;
