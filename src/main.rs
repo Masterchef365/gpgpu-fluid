c520a2b22dc474591a857fd57b39e15a61a17b97
Masterchef365 <duncan.freeman1@gmail.com>
2023-04-02 23:59:01 -0700

    flows from the grid

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use glutin::event_loop::ControlFlow;
 
 const N_PARTICLES: i32 = 1_000_000;
 const LOCAL_SIZE: i32 = 32;
-const WIDTH: i32 = 26 * LOCAL_SIZE + 1;
-const HEIGHT: i32 = 16 * LOCAL_SIZE + 1;
+const WIDTH: i32 = 24 * LOCAL_SIZE;
+const HEIGHT: i32 = 16 * LOCAL_SIZE;
 const N_ITERS: u32 = 30;
 const MAX_FINGIES: usize = 5;
 
